0aaaa4c1cde25be75ecc6b14282f146ecfc73021
boodscode237 <boodscode@gmail.com>
2024-07-28 05:01:00 +0300

    updated models and made migrations folder articles files models.py and admin.py

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Article(models.Model):
-    title = models.TextField(max_length=100)
+    title = models.CharField(max_length=100)
     content = models.TextField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(auto_now_add=False, auto_now=False, default=timezone.now, null=True, blank=True)
